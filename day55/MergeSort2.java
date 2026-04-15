bf35110942221463171471b84710c412953552f3
shivam_raj <shivamraj81020051@gmail.com>
2026-04-15 19:25:57 +0530

    merge sort my approach

--- day55/MergeSort2.java
+++ day55/MergeSort2.java
@@ -5,23 +5,52 @@ Problem: Merge Sort – LeetCode 912
 Goal: Understand recursion depth & merging logic.
  */
 public class MergeSort2 {
-    public static void mergeSort(int arr[],int left,int right){
+    public static void mergeSort(int arr[]){
+        if(arr.length<=1){return;}
 
-        int mid=(left+right)/2;
-        mergeSort(arr, left, mid);
-        mergeSort(arr, mid+1, right);
+        int mid=arr.length/2;
+        int [] left = new int[arr.length-mid];
+        int [] right = new int[mid];
+
+        for(int i=0;i<arr.length-mid;i++){
+            // System.out.println(".");
+            left[i]=arr[i];
+        }
+        for(int i=mid+1;i<arr.length;i++){
+            // System.out.println("..");
+            right[i-mid-1]=arr[i];
+        }
+
+        mergeSort(left);
+        mergeSort(right);
         merge(arr,left,right);
     }
 
-    public static int[] merge(int[] arr,int left,int right){
-        int mid = ( left + right)/2;
+    public static void merge(int[] arr,int[] left,int[] right){
+        int i=0,j=0,k=0;
 
-        return 0;
+        while(i < left.length && j < right.length){
+            if(left[i] <= right[j]){           // compare with left[i] and right[j] not left and right
+                arr[k++]=left[i++];           //compare arr and left not itself
+            }else{
+                arr[k++]=right[j++];           //compare arr and right not itself
+            }
+        }
+
+        while (i<left.length) {
+            arr[k++]=left[i++]; 
+        }
+        while (j<right.length) {
+            arr[k++]=right[j++]; 
+        }
     }
     public static void main(String[] args) {
         int [] arr = {5,8,9,88,4};
-        int left=0;
-        int right=arr.length-1;
-        mergeSort(arr,left,right);
+        mergeSort(arr);
+
+        System.out.println("sorted array: ");
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }System.out.println();
     }
 }
