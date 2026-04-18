086be0477b5e4323a7a4f7140d206dffc6ca55a1
shivam_raj <shivamraj81020051@gmail.com>
2026-04-18 21:05:19 +0530

    search of array using recursion

--- day62/RecursiononArrays.java
+++ day62/RecursiononArrays.java
@@ -58,10 +58,35 @@ public class RecursiononArrays {
         // System.out.println(i);
 
     }
+    public static boolean search2(int [] nums,int i, int t){
+        
+        if(i==nums.length-1){
+            return nums[i]==t;
+        }
+
+        return search2(nums,i+1,t);
+        // System.out.println(i);
+
+    }
+    public static boolean search3(int [] nums,int i, int t){
+        
+        if(i==nums.length-1){
+            return nums[i]==t;
+        }
+
+        if(nums[i] == t){
+            return true;
+        }
+
+        return search3(nums,i+1,t);
+        // System.out.println(i);
+
+    }
     public static void main(String[] args) {
         int [] nums = {1,2,3,4,5,6};
         
         // System.out.println("sum: "+sum2(nums,0));
-        System.out.println("max: "+max2(nums,0));
+        // System.out.println("max: "+max2(nums,0));
+        System.out.println("search: "+search3(nums,0,5));
     }
 }
