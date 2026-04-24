99dc61b1a5c95afb4cb91f68203944fb98222866
shivam_raj <shivamraj81020051@gmail.com>
2026-04-24 22:08:16 +0530

    created fun and started a for loop with array of char

--- aLeetcodePractices.java/LeetCode1047.java
+++ aLeetcodePractices.java/LeetCode1047.java
@@ -1,9 +1,34 @@
 //1047. Remove All Adjacent Duplicates In String
 
 public class LeetCode1047 {
+    public static String removeDuplicates(String s) {
+        String str = "";
+        char[] c = s.toCharArray();
+
+        for(int i= 0;i<s.length();i++){
+            
+        }
+
+        return str;
+    }
     public static void main(String[] args) {
-        System.out.println(5);
+        String s = "aaaa";
+        System.out.println(removeDuplicates(s));
     }
 }
 
+/**
+Example 1:
+Input: s = "abbaca"
+Output: "ca"
+Explanation: 
+For example, in "abbaca" we could remove "bb" since the letters are adjacent and equal, and this is the only possible move.  The result of this move is that the string is "aaca", of which only "aa" is possible, so the final string is "ca".
 
+Example 2:
+Input: s = "azxxzy"
+Output: "ay"
+
+Example 3:
+Input:  "aaaa"
+Output: ""
+ */
