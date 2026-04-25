23a4abd6e5a29d40a0de66405140c9888996b12d
shivam_raj <shivamraj81020051@gmail.com>
2026-04-25 20:47:29 +0530

    second approach using stack and string builder

--- aLeetcodePractices.java/LeetCode1047.java
+++ aLeetcodePractices.java/LeetCode1047.java
@@ -2,7 +2,7 @@
 
 public class LeetCode1047 {
     public static String removeDuplicates(String s) {
-        
+
         StringBuilder stack = new StringBuilder();
         for(Character c : s.toCharArray()){
             if(stack.length()>0 && stack.charAt(stack.length()-1)==c){
@@ -14,9 +14,25 @@ public class LeetCode1047 {
 
         return stack.toString();
     }
+    public static String removeDuplicates2(String s) {
+        char[] stack = new char[s.length()];
+        int i=0;
+
+        for(int j=0;j<s.length();j++){
+            char c = s.charAt(j);
+            if(i>0 && s.charAt(i-1)==c){
+                i--;
+            }else{
+                stack[i]=c;
+                i+=1;
+            }
+        }
+
+        return new String(stack,0,i);
+    }
     public static void main(String[] args) {
-        String s = "aaaab";
-        System.out.println("output: "+removeDuplicates(s));
+        String s = "azxxzy";
+        System.out.println("output: "+removeDuplicates2(s));
     }
 }
 
@@ -28,7 +44,7 @@ Explanation:
 For example, in "abbaca" we could remove "bb" since the letters are adjacent and equal, and this is the only possible move.  The result of this move is that the string is "aaca", of which only "aa" is possible, so the final string is "ca".
 
 Example 2:
-Input: s = "azxxzy"
+Input: s = "azxlxzy"
 Output: "ay"
 
 Example 3:
