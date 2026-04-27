eb419b2e74f228b0eb6ac954e16bc026b75fab6a
shivam_raj <shivamraj81020051@gmail.com>
2026-04-27 21:53:03 +0530

    taken input/output and created function

--- day65/leetcode78.java
+++ day65/leetcode78.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 /**
 Day 65 – Subsets & Power Set
 Concept: Recursive tree generation (include/exclude pattern).
@@ -5,8 +7,28 @@ Problem: Subsets – LeetCode 78
 Goal: Learn binary recursion pattern.
  */
 public class leetcode78 {
+    public List<List<Integer>> subsets(int[] nums) {
+        
+    }
     public static void main(String[] args) {
-        int n=3;
-        System.out.println();
+        int nums[] = {1,2,3};
+        System.out.println(subsets(nums));
     }
 }
+
+
+/**
+78. Subsets
+Given an integer array nums of unique elements, return all possible subsets (the power set).
+
+The solution set must not contain duplicate subsets. Return the solution in any order.
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+Example 2:
+
+Input: nums = [0]
+Output: [[],[0]]
+ */
