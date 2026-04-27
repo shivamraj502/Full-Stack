c76e90ca7d7cc0010e22f291c6c1995827141321
shivam_raj <shivamraj81020051@gmail.com>
2026-04-27 20:55:12 +0530

    taken input and created function

--- aLeetcodePractices.java/leetcode1021.java
+++ aLeetcodePractices.java/leetcode1021.java
@@ -1,20 +1,25 @@
 /** 1021. Remove Outermost Parentheses */
 public class leetcode1021 {
+    public static String removeOuterParentheses(String s){
+
+
+    }
     public static void main(String[] args) {
-        
+        String s = "(()())(())";
+        System.out.println(removeOuterParentheses(s));
     }
 }
 
 /**
 Example 1:
-Input: s = "(()())(())"
+Input: s = "(()()) (())"
 Output: "()()()"
 Explanation: 
 The input string is "(()())(())", with primitive decomposition "(()())" + "(())".
 After removing outer parentheses of each part, this is "()()" + "()" = "()()()".
 
 Example 2:
-Input: s = "(()())(())(()(()))"
+Input: s = "(()()) (()) (()(()))"
 Output: "()()()()(())"
 Explanation: 
 The input string is "(()())(())(()(()))", with primitive decomposition "(()())" + "(())" + "(()(()))".
