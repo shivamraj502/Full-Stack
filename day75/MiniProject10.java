d0024e4b740dcec08f941b7e0376a40395e4db3b
shivam_raj <shivamraj81020051@gmail.com>
2026-08-23 13:46:52 +0530

    Mini project 10

---
+++ day75/MiniProject10.java
@@ -0,0 +1,11 @@
+/**
+Day 75 – Mini Project #10
+Task: Build a Word Puzzle Solver using backtracking on a character grid.
+Goal: Combine recursion and pathfinding together. 
+ */
+
+public class MiniProject10{
+    public static void main(String[] args) {
+        System.out.println(5);
+    }
+}
