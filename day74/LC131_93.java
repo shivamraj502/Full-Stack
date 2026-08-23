a8815451e9f33f2da76965daee4a8c2c34dddd4a
shivam_raj <shivamraj81020051@gmail.com>
2026-08-23 09:51:04 +0530

    LC131 & LC93

--- day74/LC131_93.java
+++ day74/LC131_93.java
@@ -5,8 +5,8 @@ Palindrome Partitioning – LeetCode 131
 Restore IP Addresses – LeetCode 93
 Goal: Get comfortable with recursion + string splitting.
 */
-public class A {
+public class LC131_93 {
     public static void main(String [] raj){
-        
+        System.out.println(6);
     }
 }
