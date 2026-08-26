4a68925a82274055a5de9d0aa069f076a5e81b6b
shivam_raj <shivamraj81020051@gmail.com>
2026-08-26 15:48:16 +0530

    Mini Project 10

--- day75/MiniProject10.java
+++ day75/MiniProject10.java
@@ -5,13 +5,59 @@ Goal: Combine recursion and pathfinding together.
  */
 
 public class MiniProject10{
-    public static boolean isExist(){
+    public static boolean isExist(char[][] grid, String word){
+        int rows = grid.length;
+        int cols = grid[0].length;
 
-
-        return true;
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                if(dfs(grid, word, i, j, 0)){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
+
+    public static boolean dfs(char[][] grid, String word, int row, int col, int index){
+        if(index == word.length()){
+            return true;
+        }
+
+        if(row < 0 || col < 0 || row >= grid.length || col >= grid[0].length){
+            return false;
+        }
+
+        if(grid[row][col] != word.charAt(index)){
+            return false;
+        }
+
+        char temp = grid[row][col];
+        grid[row][col] = '#'; // mark visited
+
+        boolean found = dfs(grid, word, row+1, col, index+1) ||
+                         dfs(grid, word, row-1, col, index+1) ||
+                         dfs(grid, word, row, col+1, index+1) ||
+                         dfs(grid, word, row, col-1, index+1);
+
+        grid[row][col] = temp; // backtrack (restore)
+
+        return found;
+    }
+
     public static void main(String[] args) {
-        System.out.println(5);
+        char[][] grid = {
+                { 'A', 'B', 'C', 'E' },
+                { 'S', 'F', 'C', 'S' },
+                { 'A', 'D', 'E', 'E' }
+        };
+        String word = "ABCCED";
+
+        if (isExist(grid, word)) {
+            System.out.println("exist");
+        } else {
+            System.out.println("not exist");
+        }
     }
 }
 
