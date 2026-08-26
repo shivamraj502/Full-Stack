8ef2162c2d41d751cc62602d3b6914f26ddcbe0e
shivam_raj <shivamraj81020051@gmail.com>
2026-08-26 17:42:46 +0530

    LC 1512: second approach

--- day76/LeetCode1512.java
+++ day76/LeetCode1512.java
@@ -7,6 +7,7 @@ Problem: Number of Good Pairs – LeetCode 1512
 Goal: Understand key-value concept.
  */
 
+import java.util.*;
 public class LeetCode1512 {
     public static int numIdenticalPairs(int[] nums) {
         int count = 0;
@@ -19,8 +20,40 @@ public class LeetCode1512 {
         }
         return count;
     }
+
+    public static int numIdenticalPairs2(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        int count = 0;
+
+        for(int i = 0; i < nums.length; i++) {
+            set.add(nums[i]);
+        }// for(int i : set){ System.out.println(i); }
+
+        for(int i=0;i<nums.length;i++){
+            if(set.contains(nums[i])){ count++; }
+        }
+        
+        return count;
+    }
     public static void main(String[] args) {
         int []nums = {1,2,3,1,1,3};
-        System.out.println("Good Pairs: " + numIdenticalPairs(nums));
+        // System.out.println("Good Pairs: " + numIdenticalPairs(nums));
+        System.out.println("Good Pairs: " + numIdenticalPairs2(nums));
     }
 }
+
+/**
+Example 1:
+Input: nums = [1,2,3,1,1,3]
+Output: 4
+Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+
+Example 2:
+Input: nums = [1,1,1,1]
+Output: 6
+Explanation: Each pair in the array are good.
+
+Example 3:
+Input: nums = [1,2,3]
+Output: 0
+ */
